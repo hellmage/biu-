4b0ffee8429ea0e0358fb93423e2147cfb4cf9e0
Luyun Xie <2304310@qq.com>
2016-01-25 19:49:04 +0800

    get rid of bootstrap and jquery

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import {AutoCAT} from "./autocat"
 import {ViewPort} from "./viewport"
 
-function animate() {
-  autocat.draw();
-  requestAnimationFrame(animate);
-}
-
 function commandConsumer(evt) {
   var action = null;
   switch (evt.keyCode) {
@@ -36,7 +31,6 @@ function commandConsumer(evt) {
 function registerKeyboardEvents(viewport) {
   document.addEventListener("keypress", function(evt) {
     var message = "keypressed: " + evt.which + ', ' + String.fromCharCode(evt.which) + ', ' + evt.metaKey + ', ' + evt.keyCode + ', ' + evt.charCode;
-    document.getElementById("statusline").innerHTML = message;
     console.log(message);
     commandConsumer(evt);
   });
@@ -66,19 +60,49 @@ function registerCursorEvents() {
   });
 }
 
-function ready() {
-  var plane = document.getElementById("plane");
-  var viewport = new ViewPort(plane.offsetWidth, plane.offsetHeight);
-  window.autocat = new AutoCAT(viewport, plane);
-  registerKeyboardEvents();
-  registerCursorEvents();
+function initCanvas(plane) {
+  //return an object with the dimension of the viewport of the browser
+  function getViewportDimension() {
+    var e = window, a = 'inner';
+    if (!( 'innerWidth' in window )) {
+      a = 'client';
+      e = document.documentElement || document.body;
+    }
+    return {w:e[a + 'Width'], h:e[a + 'Height']};
+  }
 
+  //apply the dimension to the canvas
+  var dim = getViewportDimension();
+  plane.style.position = "absolute";
+  plane.style.left = "5px";
+  plane.style.top = "5px";
+  plane.width = dim.w - 10;
+  plane.height = dim.h - 10;
+}
+
+function animate() {
+  autocat.draw();
+  requestAnimationFrame(animate);
+}
+
+function initAnimation() {
   window.requestAnimationFrame = window.requestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
     window.msRequestAnimationFrame ||
     function(callback) { setTimeout(callback, 1000/60); };
   animate();
+}
+
+function ready() {
+  var plane = document.getElementById("plane");
+  var viewport = new ViewPort(plane.offsetWidth, plane.offsetHeight);
+  window.autocat = new AutoCAT(viewport, plane);
+  initCanvas(plane);
+  registerKeyboardEvents();
+  registerCursorEvents();
+  initAnimation();
+  window.autocat.updateVisibleShapes();
 }
 
 if (document.readyState != 'loading'){
