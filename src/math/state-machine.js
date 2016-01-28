d0bd8c2427c4bda6e1cc916c2c5694a93680f41a
Luyun Xie <marffin@gmail.com>
2016-01-28 16:30:28 +0800

    homemade state machine

---
+++ src/math/state-machine.js
@@ -0,0 +1,63 @@
+export class StateMachine {
+  constructor() {
+    this.states = [];
+    this.transitions = {};
+    this._state = null;
+  }
+
+  state(newState, opts) {
+    var t = typeof(newState)
+    if ((t !== 'number' && t !== 'string') || (t === 'number' && isNaN(newState)))
+      throw "Invalid type of state: " + newState;
+    if (!(newState in this.states))
+      this.states.push(String(newState));
+    if (opts && opts.initial)
+      this.states.initial = newState;
+    if (opts && opts.ending)
+      this.states.ending = newState;
+    return this;
+  }
+
+  _key(evt, from) {
+    return from + evt;
+  }
+
+  event(evt, from, to) {
+    if (typeof(evt) !== 'string')
+      throw "Event must be string: " + evt;
+    if (this.states.indexOf(from) === -1 || this.states.indexOf(to) === -1)
+      throw `Unknown state: ${from}, ${to}`;
+    this.transitions[this._key(evt, from)] = to;
+    return this;
+  }
+
+  begin() {
+    if (!this.states.initial || !this.states.ending)
+      throw "Missing initial or ending state definition";
+    this._state = this.states.initial;
+  }
+
+  next(evt) {
+    if (!this.began())
+      throw "Machine not begin yet"
+    var nextState = this.transitions[this._key(evt, this._state)];
+    if (nextState === undefined)
+      return false;
+    this._state = nextState;
+    return true;
+  }
+
+  current() {
+    if (!this.began())
+      throw "Machine not begin yet"
+    return this._state;
+  }
+
+  began() {
+    return this._state !== null;
+  }
+
+  finished() {
+    return this._state === this.states.ending;
+  }
+}
---
+++ tests/math/state-machine.spec.js
@@ -0,0 +1,84 @@
+import {assert} from "../utils/assert"
+import {StateMachine} from "../../src/math/state-machine"
+
+describe("StateMachine", function() {
+  it("happy path", function() {
+    var sm = new StateMachine()
+      .state('a', {initial: true})
+      .state('b')
+      .state('c')
+      .state('d')
+      .state('e', {ending: true})
+      .event('point', 'a', 'b')
+      .event('point', 'b', 'e')
+      .event('length', 'b', 'c')
+      .event('angle', 'c', 'e')
+      .event('angle', 'b', 'd')
+      .event('length', 'd', 'e');
+    assert.false(sm.began());
+    sm.begin();
+    assert.equal(sm.current(), 'a');
+    assert.true(sm.next('point'));
+    assert.equal(sm.current(), 'b');
+    sm.next('length');
+    assert.false(sm.finished());
+    sm.next('angle');
+    assert.true(sm.finished());
+  });
+  describe("constructor", function() {
+    it("transition must be built on known states", function() {
+      assert.throws(function() {
+        var sm = new StateMachine()
+          .state('a', {initial: true})
+          .state('b', {ending: true})
+          .event('run', 'a', 'c');
+      }, /Unknown/);
+    });
+    it("initial and ending state must be present", function() {
+      assert.throws(function() {
+        var sm = new StateMachine()
+          .state('a')
+          .state('b', {ending: true})
+          .event('run', 'a', 'b');
+        sm.begin();
+      }, /initial or ending/);
+      assert.throws(function() {
+        var sm = new StateMachine()
+          .state('a', {initial: true})
+          .state('b')
+          .event('run', 'a', 'b');
+        sm.begin();
+      }, /initial or ending/);
+    });
+  });
+  describe(".next", function() {
+    it("machine must begin first", function() {
+      assert.throws(function() {
+        var sm = new StateMachine()
+          .state('a', {initial: true})
+          .state('b', {ending: true})
+          .event('run', 'a', 'b');
+        sm.next('run');
+      }, /not begin/);
+    });
+    it("fails for unknown event", function() {
+      var sm = new StateMachine()
+        .state('a', {initial: true})
+        .state('b', {ending: true})
+        .event('run', 'a', 'b');
+      sm.begin();
+      assert.false(sm.next('rush'));
+    });
+  })
+  describe(".current", function() {
+    it("machine must begin first", function() {
+      assert.throws(function() {
+        var sm = new StateMachine()
+          .state('a', {initial: true})
+          .state('b', {ending: true})
+          .event('run', 'a', 'b');
+        sm.current();
+      }, /not begin/);
+    });
+  })
+});
